56b85f4755d27a0b0435d65b3e016452a26fd73e
Dhanish-Raza <adhanishraza7@gmail.com>
2025-12-25 01:41:38 +0530

    hai

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "ResumeAi" },
     { name: "description", content: "Smart feedback for your dream job!" },
@@ -9,17 +11,25 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-      <Navbar/>
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
 
       <section className="main-section">
-          <div className="page-heading">
-              <h1>Track Your Application & Resume Ratings</h1>
-              <h2>Review your submission and check AI-powered feedback.</h2>
-
+        <div className="page-heading">
+          <h1>Track Your Application & Resume Ratings</h1>
+          <h2>Review your submission and check AI-powered feedback.</h2>
+        </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
           </div>
-
+        )}
       </section>
 
-  </main>
+
+    </main>
+  );
 }
